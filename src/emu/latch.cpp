399661d64e6c9c050b93d3533d20769508772aa2
crankery <dave@crankery.com>
2026-03-18 23:24:47 -0400

    checkpoint

---
+++ src/emu/latch.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "latch.h"
+
+void Latch::out(uint8_t addr, uint8_t value)
+{
+    value_ = value;
+
+    printf("latch %1x aet %02x\n", offset_, value_);
+}
---
+++ src/emu/latch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstring>
+#include "memoryIODevice.h"
+
+// simple write only latch
+class Latch : public MemoryIODevice
+{
+public:
+    Latch(const uint8_t slot, const uint8_t offset)
+        : MemoryIODevice(slot, offset, 1)
+    {
+        value_ = 0;
+    }
+
+    virtual void out(uint8_t addr, uint8_t value) override;
+
+    uint8_t getValue()
+    {
+        return value_;
+    }
+
+private:
+    uint8_t value_;
+};
