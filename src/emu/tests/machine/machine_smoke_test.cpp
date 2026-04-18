6e09943323736f209042e1a05ba4882da7ae0496
crankery <dave@crankery.com>
2026-04-18 19:33:18 -0400

    integrate gtest

---
+++ src/emu/tests/machine/machine_smoke_test.cpp
@@ -0,0 +1,11 @@
+// tests/machine/Machine_smoke_test.cpp
+#include <gtest/gtest.h>
+
+#include "machine/machine.h"
+
+TEST(MachineSmokeTest, CanConstructSystemUnderTest)
+{
+    Machine sut;
+
+    EXPECT_TRUE(true);
+}
